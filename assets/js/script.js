56f2441bd919fd4049c8e158df7037b0b0f7072c
FonKnp <fonnfonk@gmail.com>
2024-02-23 13:58:36 -0500

    add quiz variables with arrays

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const quiz = [
+  {
+    question: 'What HTML tag element do we put the JavaScript in?',
+    choices: ['<script>','<javascript>','<link>','<link rel=javascript>'],
+    answer: ''
+  },
+  {
+    question: '',
+    choices: ['','','',''],
+    answer: ''
+  },
+  {
+    question: '',
+    choices: ['','','',''],
+    answer: ''
+  },
+  {
+    question: '',
+    choices: ['','','',''],
+    answer: ''
+  },
+]
+
+var questionAll = document.getElementById('question');
+var choicesAll = document.getElementById('choices');
+var resultAll = document.getElementById('result');
